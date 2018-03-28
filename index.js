e664cb7d10710dd8cac0a66f7baf71d329ba8691
mkhan <mkhan@allhomes.com.au>
2018-03-28 12:36:50 +1100

    log sources

--- index.js
+++ index.js
@@ -33,8 +33,9 @@ const Residue = function(options) {
     }
     residue_internal.connect();
     this.logger = residue_internal.getLogger(options.logger_id);
-    // add one level as we pick up from further source
-    this.logger._source_base_index = this.logger._source_base_index + (options.source_layer_count || 6);
+    this.logger.log_sources = options.log_sources || {
+        base_idx: this.logger.log_sources + (options.log_source_layer_count || 6),
+    };
 };
 
 // Inherit from `winston.Transport`
